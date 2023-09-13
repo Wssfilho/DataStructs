f8e28884e1ad5875c7d5411fddc9cdbe9fba3af5
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-09-13 07:39:58 -0300

    modified:   Estrutura de Dados/listaentregaq.c 	modified:   Estrutura de Dados/output/listaentregaq.exe

--- Estrutura de Dados/listaentregaq.c
+++ Estrutura de Dados/listaentregaq.c
@@ -3,19 +3,17 @@
 #include <string.h>
 #define STACKSIZE 26
 
-typedef struct 
+typedef struct
 {
     int top;
     char vagoes[STACKSIZE];
-}Stack;
+} Stack;
 
-// verifica se pilha está vazia
 int empty(Stack *ps);
-// remove e retorna item do topo da pilha;
 int pop(Stack *ps);
-// insere intem x no topo da pilha
 void push(Stack *ps, int x);
 char *alocar(int n);
+
 int main(void)
 {
     int n;
@@ -23,57 +21,87 @@ int main(void)
     Stack trem_esq, trem_dir;
     trem_dir.top = -1;
     trem_esq.top = -1;
+
     printf("Insira o tamanho do vagao: ");
     scanf("%d", &n);
     fflush(stdin);
+
     char *vagoes = alocar(n);
+
     printf("Insira os vagoes: ");
     fgets(vagoes, n, stdin);
     fflush(stdin);
+
     printf("Insira as operacoes: ");
     fgets(operacoes, STACKSIZE, stdin);
     fflush(stdin);
-    
+
     int i, j = 0;
+
     for (i = 0; operacoes[i] != '\0'; i++)
     {
         if (operacoes[i] == 'E')
         {
-            push(&trem_esq, vagoes[j]);
-            j++;
+            if (j < n)
+            {
+                push(&trem_esq, vagoes[j]);
+                j++;
+            }
+            else
+            {
+                printf("Não há mais vagões para mover.\n");
+                break;
+            }
         }
         else if (operacoes[i] == 'D')
         {
-            push(&trem_dir, pop(&trem_esq));
+            if (!empty(&trem_esq))
+            {
+                push(&trem_dir, pop(&trem_esq));
+            }
+            else
+            {
+                printf("Não há mais vagões para mover.\n");
+                break;
+            }
         }
     }
+
     printf("\n");
+
     for (int i = 0; i <= trem_dir.top; i++)
     {
         printf("%c ", trem_dir.vagoes[i]);
     }
+
     free(vagoes);
+
     return 0;
 }
+
 char *alocar(int n)
 {
     char *temp;
     temp = malloc(n * sizeof(char));
+
     if (temp == NULL)
         exit(-1);
 
     return temp;
 }
+
 int empty(Stack *ps)
 {
     if (ps->top == -1)
         return 1;
+
     return 0;
 }
 
 int pop(Stack *ps)
 {
     int aux;
+
     if (empty(ps))
     {
         printf("ERRO- PILHA VAZIA");
@@ -82,6 +110,7 @@ int pop(Stack *ps)
 
     aux = ps->vagoes[ps->top];
     ps->top--;
+
     return aux;
 }
 
