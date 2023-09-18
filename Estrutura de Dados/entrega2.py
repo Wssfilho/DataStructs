c85c9bb15d771333c120264f5f56628cf6a89eac
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-09-18 19:01:37 -0300

    Changes to be committed: 	new file:   Estrutura de Dados/entrega2.py

---
+++ Estrutura de Dados/entrega2.py
@@ -0,0 +1,13 @@
+
+while True:
+        operacao = input("Insira a operacao E para enfilerar e D para desenfilerar")
+        if operacao == 'E':
+        
+        elif operacao == 'D':
+       
+        elif operacao == '-':
+             return False 
+        
+def enfilerar(valor):
+       
+    
