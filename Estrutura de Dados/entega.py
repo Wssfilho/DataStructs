217c57996b6dad66eee4afb74d3f384cbcb88cc1
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-09-21 09:26:36 -0300

    changes

--- Estrutura de Dados/entega.py
+++ Estrutura de Dados/entega.py
@@ -6,7 +6,12 @@ class Fila:
         self.fila.append(valor)
 
     def desenfileirar(self):
-        return self.fila.pop()
+        if len(self.fila) > 0:
+            max_valor = max(self.fila)
+            max_index = self.fila.index(max_valor)
+            self.fila.pop(max_index)
+        else:
+            print("A fila está vazia!")
 
     def __str__(self):
         return ' '.join(str(i) for i in self.fila)
