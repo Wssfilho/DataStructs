e2874384f5deaea43ea5540abcf41eefd7567680
Wssfilho <wilsonsa004@gmail.com>
2023-09-25 10:43:49 -0300

    modified:   pypoo.py

--- pypoo.py
+++ pypoo.py
@@ -1,32 +1,29 @@
 class Fila:
     def __init__(self):  # construtor do py
-        self.fila = []
+        self.fila = [] #cria a fila
 
-    def enfileirar(self, valor):
+    def enfileirar(self, valor): #funcao para colocar um elemento na fila
         self.fila.append(valor)
 
-    def desenfileirar(self):
-        if len(self.fila) > 0:
-            max_valor = max(self.fila)
-            max_index = self.fila.index(max_valor)
-            self.fila.pop(max_index)
+    def desenfileirar(self): #funcao desenfilerar
+        if len(self.fila) > 0: #verifica se a fila não está vazia
+            max_valor = max(self.fila) #pega o maior valor da fila
+            max_index = self.fila.index(max_valor) #pega o index do maior valor
+            self.fila.pop(max_index) # remove o maior valor da fila
         else:
-            print("A fila está vazia!")
-
-    def __str__(self):
-        return ' '.join(str(i) for i in self.fila)
+            print("A fila está vazia!") #printa se a fila esta vazia
 
 
-fila = Fila()
+fila = Fila() #cria a fila
 
-while True:
+while True: #enquanto for diferente de '-'
     operacao = input(
         "Digite a operação (E para enfileirar, D para desenfileirar, - para sair): ")
-    if operacao == 'E':
+    if operacao == 'E': #se a operacao for igual a 'E'
         valor = int(input("Digite o valor a ser enfileirado: "))
         fila.enfileirar(valor)
-    elif operacao == 'D':
+    elif operacao == 'D': #se a operacao for igual a 'D'
         fila.desenfileirar()
     elif operacao == '-':
-        break
+        break #para se for igual a '-'
     print(fila)
