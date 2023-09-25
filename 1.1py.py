f2f05055d1f41f1899addfd60c879acc3f0f21cd
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-09-25 14:32:32 -0300

    Changes to be committed: 	modified:   1.1py.py

--- 1.1py.py
+++ 1.1py.py
@@ -1,2 +1,29 @@
-def criar():
-    lista_inic
+# #Dada uma lista de números, crie duas outras listas para separar os números pares dos ímpares
+# exemplo: 
+# lista_inicial=[10, 20, 33, 44, 55, 56, 66, 89]
+# lista_par=[10, 20, 44, 56, 66]
+# lista_impar=[33, 55, 89]
+
+def inserir(valor):
+    lista_inicial.append(valor)
+
+
+def separar():
+    while len(lista_inicial) > 0:
+        a = lista_inicial[0] % 2
+        if a == 0:
+            b = lista_inicial.pop(0)
+            lista_par.append(b)
+        else:
+            c = lista_inicial.pop(0)
+            lista_impar.append(c)
+
+
+lista_inicial = [10, 20, 33, 44, 55, 56, 66, 89]
+lista_par = []
+lista_impar = []
+
+separar()
+print(lista_inicial)
+print(lista_par)
+print(lista_impar)
