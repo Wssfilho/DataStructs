a36c28daff2e01bf1ab6d59b74c4268989b0c202
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-10-19 17:05:20 -0300

    modified:   Listas Circular e Duplamente encadeadas/5.2.py

--- Listas Circular e Duplamente encadeadas/5.2.py
+++ Listas Circular e Duplamente encadeadas/5.2.py
@@ -1,23 +1,23 @@
 class No:
     """Esta classe representa um nó/elemento de uma lista encadeada."""
+
     def __init__(self, valor, proximo=None, anterior=None):
         self.valor = valor
         self.prox = proximo
         self.ant = anterior
 
+
 class Lista:
     """Esta classe contem funções para a manipulação de lista encadeada."""
-    
-    inicio=None
 
+    inicio = None
 
-    
-    def inserirInicio (self, novo: No):
+    def inserirInicio(self, novo: No):
         ult = self.inicio
-        if self.inicio==None:
+        if self.inicio == None:
             self.inicio = novo
             self.inicio.prox = self.inicio
-            #print(self.inicio.valor)
+            # print(self.inicio.valor)
         else:
             novo.prox = self.inicio
             self.inicio.ant = novo
@@ -25,34 +25,29 @@ class Lista:
                 ult = ult.prox
             ult.prox = novo
             novo.ant = ult
-                
-        
-       
 
-    
-    def mostrar (self):
-        atual=self.inicio
+    def mostrar(self):
+        atual = self.inicio
         while True:
             print(atual.valor, end=' ')
-            atual=atual.prox
-            if atual==self.inicio:
+            atual = atual.prox
+            if atual == self.inicio:
                 return False
 
 
-#inicializa uma lista vazia
-lista_1=Lista()
+# inicializa uma lista vazia
+lista_1 = Lista()
 
 # Adiciona alguns nós
-novo=No(10)
+novo = No(10)
 lista_1.inserirInicio(novo)
 
-novo=No(13)
+novo = No(13)
 lista_1.inserirInicio(novo)
 
-novo=No(20)
+novo = No(20)
 lista_1.inserirInicio(novo)
 
 lista_1.mostrar()
 
 # removendo e mostrando estado da lista
-
