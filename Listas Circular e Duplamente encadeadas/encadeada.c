25e7a306d7be67dd536618cdf0f8908d06827baa
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-10-25 21:48:51 -0300

    new file:   Listas Circular e Duplamente encadeadas/encadeada.c 	new file:   Pilha e Fila/fila.c 	new file:   Pilha e Fila/output/fila.exe 	new file:   prova/1.1.c

---
+++ Listas Circular e Duplamente encadeadas/encadeada.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct elemento
+{
+    int valor;
+    struct elemento *prox;
+    struct elemento *ant;
+};
+typedef struct elemento no;
+typedef struct elemento *Pno;
+
+int vazia(Pno inicio)
+{
+    if (inicio == NULL)
+        return 1;
+    else
+        return 0;
+}
+Pno criarElemento(int valor)
+{
+    Pno novo;                       // CRIACAO DE ESTRUTURA DO TIPO PONTEIRO COM NOME NOVO
+    novo = (Pno)malloc(sizeof(no)); // ALOCA (1) ESPACO NA MEMORIA E APONTA AUTOMATICAMENTE DO TAMANHO DA ESTRUTURA
+    if (novo == NULL)
+        exit(0);         // VERIFICA SE ALOCOU CERTO
+    novo->valor = valor; // ENTAO ELE APONTA PARA O VALOR E COLOCA O VALOR
+    novo->prox = NULL;   // ELE APONTA PARA O NULLO ESTANDO PREPARADO PARA ALOCAR OUTRO ESPACO
+    novo->ant = NULL;
+    return novo; // RETORNA UM PONTEIRO PARA A NOVA ESTRUTUA QUE FOI ALOCADA
+}
+void inserirIn(Pno *inicio, Pno *novo)
+{
+    if (vazia(inicio))
+    {
+        *inicio = *novo;
+    }
+    else
+    {
+        (*novo)->prox = *inicio;
+        (*inicio)->ant = *novo;
+        *inicio = *novo;
+    }
+}
+void inserefim(Pno *inicio, Pno *novo)
+{
+    if (vazia(inicio))
+    {
+        *inicio = *novo;
+    }
+    else
+    {
+        Pno ultimo = *inicio;
+        while(ultimo->prox != NULL)
+        {
+            ultimo = ultimo->prox;
+        }
+        ultimo->prox = *novo;
+        (*novo)->ant = ultimo;
+    }
+}
