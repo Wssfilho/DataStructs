07d38e2588f1d61b814b79f8a66af718cb75d23b
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-11-16 12:16:46 -0300

    Add No and Arvore classes to trabalho.py

---
+++ Listas Circular e Duplamente encadeadas/Arvores/trabalho.py
@@ -0,0 +1,8 @@
+class No:
+        def __init__(self, valor, esquerda=None, direita=None):
+            self.valor = valor
+            self.esquerda = esquerda
+            self.direita = direita
+class Arvore:
+        
+               
