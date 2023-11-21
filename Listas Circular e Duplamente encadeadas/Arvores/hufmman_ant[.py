51215d6932825829960f24d307cb4036efed2ca6
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-11-21 18:59:58 -0300

    Refactor Huffman encoding function

--- Listas Circular e Duplamente encadeadas/Arvores/hufmman_ant[.py
+++ Listas Circular e Duplamente encadeadas/Arvores/hufmman_ant[.py
@@ -2,61 +2,60 @@
 class No:
     def __init__(self, caractere, frequencia):
         self.caractere = caractere
-        self.frequencia = frequencia
+        self.freq = frequencia
         self.esquerda = None
         self.direita = None
 
 # Função principal para a codificação de Huffman
-def codificacao_huffman(texto):
+def huff(texto: str):
     # Criando um dicionário para armazenar a frequência de cada caractere
-    dicionario_frequencia = {}
+    freqcaractere = {}
     for caractere in texto:
-        if caractere not in dicionario_frequencia:
-            dicionario_frequencia[caractere] = 0
-        dicionario_frequencia[caractere] += 1
+        if caractere not in freqcaractere:
+            freqcaractere[caractere] = 0
+        freqcaractere[caractere] += 1
 
     # Imprimindo a frequência de cada caractere
-    print(f"Frequência de cada caractere: {dicionario_frequencia}")
+    print(f"Frequência de cada caractere: {freqcaractere}")
 
     # Criando uma lista de nós
-    nos = [No(caractere, frequencia) for caractere, frequencia in dicionario_frequencia.items()]
+    arvore_nos = [No(caractere, frequencia) for caractere, frequencia in freqcaractere.items()]
 
     # Loop para construir a árvore de Huffman
-    while len(nos) > 1:
+    while len(arvore_nos) > 1:
         # Ordenando os nós por frequência
-        nos.sort(key=lambda x: x.frequencia)
+        arvore_nos.sort(key=lambda x: x.freq)
 
         # Removendo os dois nós com menor frequência
-        no_esquerda = nos.pop(0)
-        no_direita = nos.pop(0)
+        no_esquerda = arvore_nos.pop(0)
+        no_direita = arvore_nos.pop(0)
 
         # Criando um novo nó que combina os dois nós removidos
-        no_merge = No(None, no_esquerda.frequencia + no_direita.frequencia)
-        no_merge.esquerda = no_esquerda
-        no_merge.direita = no_direita
+        juncao_menores = No(None, no_esquerda.freq + no_direita.freq)
+        juncao_menores.esquerda = no_esquerda
+        juncao_menores.direita = no_direita
 
         # Adicionando o novo nó à lista de nós
-        nos.append(no_merge)
+        arvore_nos.append(juncao_menores)
 
     # A árvore de Huffman é o último nó na lista
-    arvore_huffman = nos[0]
+    arvore_huffman = arvore_nos[0]
 
     # Dicionário para armazenar o código Huffman para cada caractere
     dicionario_codigo = {}
 
     # Função recursiva para gerar o código Huffman para cada caractere
-    def obter_codigo(no, codigo_atual=""):
+    def codigo(no, codigo_atual=""):
         if no is None:
             return
-
         if no.caractere is not None:
             dicionario_codigo[no.caractere] = codigo_atual
 
-        obter_codigo(no.esquerda, codigo_atual + "0")
-        obter_codigo(no.direita, codigo_atual + "1")
+        codigo(no.esquerda, codigo_atual + "0")
+        codigo(no.direita, codigo_atual + "1")
 
     # Chamando a função obter_codigo para preencher o dicionário dicionario_codigo
-    obter_codigo(arvore_huffman)
+    codigo(arvore_huffman)
 
     # Codificando o texto usando o dicionário dicionario_codigo
     texto_codificado = "".join([dicionario_codigo[caractere] for caractere in texto])
@@ -68,6 +67,6 @@ def codificacao_huffman(texto):
 texto = input("Digite o texto aqui: ")
 
 # Chamando a função codificacao_huffman e imprimindo os resultados
-arvore, dicionario, codificado = codificacao_huffman(texto)
+arvore, dicionario, codificado = huff(texto)
 print(f"Código Huffman para cada caractere: {dicionario}")
 print(f"Texto codificado: {codificado}")
