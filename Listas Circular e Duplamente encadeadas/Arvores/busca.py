7b426092b25201fc9bd3929e447b26e1c876c7f7
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-11-22 17:49:56 -0300

    Add new trabalho

--- Listas Circular e Duplamente encadeadas/Arvores/busca.py
+++ Listas Circular e Duplamente encadeadas/Arvores/busca.py
@@ -1,34 +1,47 @@
 class No:
-    def __init__(self, nome, chave, esquerda, direita, cpf) -> None:
-        self.chave = chave
-        self.esq = esquerda
-        self.dir = direita
-        self.nome = nome
+    def __init__(self, cpf, nome):
         self.cpf = cpf
+        self.nome = nome
+        self.esquerda = None
+        self.direita = None
+        self.profundidade = 1
 
-class Arvore:
-    def buscar(raiz, chave):
-        if raiz is None or chave == raiz.chave:
-            return raiz
-        if chave < raiz.chave:
-            return Arvore.buscar(raiz.esq, chave)
+
+def inserir(raiz, cpf, nome):
+    if raiz is None:
+        return No(cpf, nome)
+    else:
+        if cpf < raiz.cpf:
+            raiz.esquerda = inserir(raiz.esquerda, cpf, nome)
+            raiz.esquerda.profundidade = raiz.profundidade + 1
         else:
-            return Arvore.buscar(raiz.dir, chave)
-        
-    def inserir(raiz, chave):
-        if raiz is None:
-            return No(chave)
-        if chave < raiz.chave:
-            raiz.esq = Arvore.inserir(raiz.esq, chave)
-        else:
-            raiz.dir = Arvore.inserir(raiz.dir, chave)
+            raiz.direita = inserir(raiz.direita, cpf, nome)
+            raiz.direita.profundidade = raiz.profundidade + 1
         return raiz
-    def pre_ordem(self: No):  # function para imprimir a arvore indo da raiz para a esquerda depois para a direita
-        if self is not None:
-            print(self.valor, end='->')
-            Arvore.pre_ordem(self.esquerda)
-            Arvore.pre_ordem(self.direita)
-            
-for i in range(5):
-    a = No(i)
-    Arvore.inserir(a, 1)
+
+
+def buscar(raiz, cpf):
+    if raiz is None or raiz.cpf == cpf:
+        return raiz
+    if cpf < raiz.cpf:
+        return buscar(raiz.esquerda, cpf)
+    return buscar(raiz.direita, cpf)
+
+
+def main():
+    raiz = None
+    n = int(input())
+    for _ in range(n):
+        operacao = input().split()
+        if operacao[0] == 'I':
+            cpf = int(operacao[1])
+            nome = input()
+            raiz = inserir(raiz, cpf, nome)
+        elif operacao[0] == 'B':
+            cpf = int(operacao[1])
+            no = buscar(raiz, cpf)
+            print(f'{no.nome} {no.profundidade}')
+
+
+if __name__ == "__main__":
+    main()
