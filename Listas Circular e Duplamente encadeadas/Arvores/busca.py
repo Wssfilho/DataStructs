9b6d0156f679e59a739981c41c6f1162ca5db4c5
Wssfilho <wilsonsa004@gmail.com>
2023-11-26 21:16:29 -0300

    Refactor tree depth variable name

--- Listas Circular e Duplamente encadeadas/Arvores/busca.py
+++ Listas Circular e Duplamente encadeadas/Arvores/busca.py
@@ -5,30 +5,30 @@ class No:
         self.nome = nome  # Nome do nó
         self.esquerda = None  # Nó filho à esquerda
         self.direita = None  # Nó filho à direita
-        self.profundidade = 1  # Profundidade do nó na árvore
+        self.prof = 1  # Profundidade do nó na árvore
 
-
+class Arvore: 
 # Função para inserir um novo nó na árvore
-def inserir(raiz, cpf, nome):
-    if raiz is None:  # Se a árvore estiver vazia, cria um novo nó
-        return No(cpf, nome)
-    else:
-        if cpf < raiz.cpf:  # Se o CPF for menor, insere à esquerda
-            raiz.esquerda = inserir(raiz.esquerda, cpf, nome)
-            raiz.esquerda.profundidade = raiz.profundidade + 1  # Atualiza a profundidade
-        else:  # Se o CPF for maior, insere à direita
-            raiz.direita = inserir(raiz.direita, cpf, nome)
-            raiz.direita.profundidade = raiz.profundidade + 1  # Atualiza a profundidade
-        return raiz  # Retorna a raiz da árvore
+    def inserir(raiz, cpf, nome):
+        if raiz is None:  # Se a árvore estiver vazia, cria um novo nó
+            return No(cpf, nome)
+        else:
+            if cpf < raiz.cpf:  # Se o CPF for menor, insere à esquerda
+                raiz.esquerda = Arvore.inserir(raiz.esquerda, cpf, nome)
+                raiz.esquerda.prof = raiz.prof + 1  # Atualiza a profundidade
+            else:  # Se o CPF for maior, insere à direita
+                raiz.direita = Arvore.inserir(raiz.direita, cpf, nome)
+                raiz.direita.prof = raiz.prof + 1  # Atualiza a profundidade
+            return raiz  # Retorna a raiz da árvore
 
 
-# Função para buscar um nó na árvore
-def buscar(raiz, cpf):
-    if raiz is None or raiz.cpf == cpf:  # Se a árvore estiver vazia ou o CPF for encontrado, retorna o nó
-        return raiz
-    if cpf < raiz.cpf:  # Se o CPF for menor, busca à esquerda
-        return buscar(raiz.esquerda, cpf)
-    return buscar(raiz.direita, cpf)  # Se o CPF for maior, busca à direita
+    # Função para buscar um nó na árvore
+    def buscar(raiz, cpf):
+        if raiz is None or raiz.cpf == cpf:  # Se a árvore estiver vazia ou o CPF for encontrado, retorna o nó
+            return raiz
+        if cpf < raiz.cpf:  # Se o CPF for menor, busca à esquerda
+            return Arvore.buscar(raiz.esquerda, cpf)
+        return Arvore.buscar(raiz.direita, cpf)  # Se o CPF for maior, busca à direita
 
 
 # Inicializando a árvore
@@ -39,8 +39,8 @@ for i in range(n):
     if operacao[0] == 'I':  # Se a operação for de inserção
         cpf = int(operacao[1])  # Lendo o CPF
         nome = input()  # Lendo o nome
-        raiz = inserir(raiz, cpf, nome)  # Inserindo o nó na árvore
+        raiz = Arvore.inserir(raiz, cpf, nome)  # Inserindo o nó na árvore
     elif operacao[0] == 'B':  # Se a operação for de busca
         cpf = int(operacao[1])  # Lendo o CPF
-        no = buscar(raiz, cpf)  # Buscando o nó na árvore
-        print(f'{no.nome} {no.profundidade}')  # Imprimindo o nome e a profundidade do nó
+        no = Arvore.buscar(raiz, cpf)  # Buscando o nó na árvore
+        print(f'{no.nome} {no.prof}')  # Imprimindo o nome e a profundidade do nó
