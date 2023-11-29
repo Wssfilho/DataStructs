a2523f54086405dbd010837e6ad0882821644b24
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-11-29 20:44:28 -0300

    Fix tree traversal bug and add test cases

--- Listas Circular e Duplamente encadeadas/Arvores/3.a.py
+++ Listas Circular e Duplamente encadeadas/Arvores/3.a.py
@@ -21,3 +21,19 @@ class No:
             return self.pai
         else: 
             return self.pai.ancestral_esq(self.pai)
+        
+# Criando alguns nós
+no1 = No(1, None, None, None)
+no3 = No(3, None, None, None)
+no2 = No(2, no1, no3, None)
+
+# Atualizando os pais dos nós
+no1.pai = no2
+no3.pai = no2
+
+# Agora no2 é a raiz da árvore, e no1 e no3 são seus filhos
+arvore = no2
+
+# Você pode agora chamar os métodos na raiz da árvore
+print(arvore.maximo().valor)  # Deve imprimir 3
+print(arvore.antecessor().valor)  # Deve imprimir 1
