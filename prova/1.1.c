fa4134a12a1c468039b7e28987d877f378555d3b
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-12-17 10:55:59 -0300

    Update launch.json and 1.1.c files

--- prova/1.1.c
+++ prova/1.1.c
@@ -1,30 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define STACKSIZE 4
-typedef struct
+int **alocar(int n)
 {
-    int item;
-    struct stack *prox;
-    struct stack *ant;
-}No;
-typedef struct filha
-{
-    No *top;
-    No *ult;
-}Filha;
-void remove(Filha *inicio);
-// typedef struct stack no;
-// typedef struct stack *Pno;
-
-// int remove(Pno *inicio)
-// {
-//     if((*inicio)->top == -1)
-//     {
-//         return 0;
-//     }
-//     else
-//     {
-        
-//     }
-// }
+    
+}
