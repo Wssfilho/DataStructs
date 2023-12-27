aa75c28a23c413eb7a8557dc4a593f12549e9172
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-12-27 15:11:05 -0300

    Update user authentication logic

--- Listas Circular e Duplamente encadeadas/cola.py
+++ Listas Circular e Duplamente encadeadas/cola.py
@@ -1,49 +1,15 @@
-class No:
-    def __init__(self, dado=None):
-        self.dado = dado
-        self.proximo = None
-        self.anterior = None
+class pessoa:
+    def __init__(self, nome, idade) -> None:
+        self.nome = nome
+        self.idade = idade
+        
+    def mostrar(self) -> None:
+        print(self.nome)
+        print(self.idade)
+        
 
-
-class ListaDuplamenteEncadeada:
-    def __init__(self):
-        self.cabeca = None
-
-    def adicionar(self, dado):
-        if not self.cabeca:
-            self.cabeca = No(dado)
-        else:
-            atual = self.cabeca
-            while atual.proximo:
-                atual = atual.proximo
-            atual.proximo = No(dado)
-            atual.proximo.anterior = atual
-
-    def exibir(self):
-        elementos = []
-        no_atual = self.cabeca
-        while no_atual:
-            elementos.append(no_atual.dado)
-            no_atual = no_atual.proximo
-        return elementos
-
-    def mover_para_frente(self, i):
-        atual = self.cabeca
-        contador = 0
-        while atual and contador < i:
-            atual = atual.proximo
-            contador += 1
-        if atual is None or atual.proximo is None:
-            return
-        proximo_no = atual.proximo
-        if atual.anterior is None:
-            self.cabeca = proximo_no
-            proximo_no.anterior = None
-        else:
-            atual.anterior.proximo = proximo_no
-            proximo_no.anterior = atual.anterior
-        atual.proximo = proximo_no.proximo
-        if proximo_no.proximo is not None:
-            proximo_no.proximo.anterior = atual
-        proximo_no.proximo = atual
-        atual.anterior = proximo_no
+nome = input("insira o nome")
+idade = input("insira a idade")
+pessoa = pessoa(nome, idade)
+pessoa.mostrar()
+        
